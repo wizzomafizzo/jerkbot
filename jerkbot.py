cfb1f70237c1f20c8694e6cb9c8ebfcf643a30b2
Callan Barrett <callan@zoocar.org>
2015-09-07 20:36:03 +0800

    Lots of shit

--- jerkbot.py
+++ jerkbot.py
@@ -17,54 +17,29 @@ You'll need:
 - PhantomJS (you can download static builds off their site)
 """
 
-import logging, logging.handlers, os, sys
-import re, sqlite3, time
-import praw, pyimgur
+import logging
+import logging.handlers
+import os
+import sys
+import random
+import urllib.request
+import re
+import sqlite3
+import time
+import praw
+import pyimgur
 
 from PIL import Image
 from selenium import webdriver
 
-from config import CONFIG
+from config import CONFIG as C
+from config import TEMPLATES as T
 
-# notification templates
-TEMPLATES = {
-    "sketchy_subject": "sketchy user detected",
-    "sketchy_body": """someone submitted with a sketchy account (low karma, newly created)
-
-- username: %s
-- info: %s
-- submission: %s
-
-[ban them](http://www.reddit.com/message/compose/?to=jerkbot-3hunna&subject=shadowban&message=ban%%20%s) | [show ban list](http://www.reddit.com/message/compose/?to=jerkbot-3hunna&subject=shadowban&message=showbans)
----|---
-""",
-    "non-np_subject": "removed a non-np submission",
-    "non-np_body": "this one: %s",
-    "non-np_comment": """fam, you need to submit this as an np link. this submission has been removed. you can read why [here](http://www.reddit.com/r/Hiphopcirclejerk/comments/297w9r/actually_serious_new_rule_enforcement_starting_on/).
-
-you can resubmit with this link:
-
-%s
-""",
-    "screenshot_comment": "screenshot: %s",
-    "ban_subject": "shadowbans updated",
-    "ban_body": "%s has been shadowbanned by %s",
-    "unban_body": "%s had their shadowban removed by %s",
-    "showbans_body": """shadowban list requested by %s
-
-[ban new user](http://www.reddit.com/message/compose/?to=jerkbot-3hunna&subject=shadowban&message=ban%%20username) | [unban user](http://www.reddit.com/message/compose/?to=jerkbot-3hunna&subject=shadowban&message=unban%%20username)
----|---
-
----
-
-%s
-"""
-}
 
 # set up fancy logging shit
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-rotate = logging.handlers.RotatingFileHandler(CONFIG["log_file"],
+rotate = logging.handlers.RotatingFileHandler(C["log_file"],
                                               maxBytes=1024*1000,
                                               backupCount=5)
 rotate.setFormatter(logging.Formatter("%(asctime)s %(levelname)-8s %(message)s"))
@@ -74,16 +49,12 @@ console.setLevel(logging.INFO)
 console.setFormatter(logging.Formatter("%(levelname)-8s %(message)s"))
 logger.addHandler(console)
 
-# make the image dir, parent dir must exist
-if not os.path.isdir(CONFIG["image_dir"]):
-    os.mkdir(CONFIG["image_dir"])
-    logging.info("Created new image_dir: %s", CONFIG["image_dir"])
 
-# this database keeps track of processed submissions/comments, so work is not doubled up
-# and also maintains a shitlist of users
+# this database keeps track of processed submissions/comments,
+# so work is not doubled up, and also maintains a shitlist of users
 class JerkDB():
     def __init__(self):
-        self.db_filename = CONFIG["db_file"]
+        self.db_filename = C["db_file"]
 
         if not os.path.isfile(self.db_filename):
             self.init_db()
@@ -118,7 +89,8 @@ class JerkDB():
         return c.fetchall()
 
     def already_done(self, name):
-        q = "select name from submissions where name = ? and (status = ? or status = ?)"
+        q = """select name from submissions where name = ? and
+        (status = ? or status = ?)"""
         c = self.db.cursor()
         c.execute(q, (name, "complete", "failed"))
         if c.fetchone() == None:
@@ -207,10 +179,19 @@ class JerkDB():
         else:
             return True
 
+
+def grab_quote():
+    """Pick a random line from the configured file."""
+    content = open(C["random_comment_file"], encoding="utf-8")
+    quotes = content.read().split("\n")
+    content.close()
+
+    return random.choice(quotes)
+
 def should_screenshot(url):
     """Return true if submission is a comment link not in the same sub."""
     comment_pattern = "reddit.com/r/\w+/comments/\w+"
-    subreddit_pattern = "/%s/" % (CONFIG["subreddit"])
+    subreddit_pattern = "/%s/" % (C["subreddit"])
 
     # we're only interested in comments and self posts
     if re.search(comment_pattern, url, re.I) != None:
@@ -222,7 +203,7 @@ def should_screenshot(url):
 
 def is_np(url):
     """Return true if url is a no participate link."""
-    if re.match("https?://.*\.?np\.", url):
+    if re.match("https?://np\.", url):
         return True
     else:
         return False
@@ -232,23 +213,25 @@ def to_np(url):
     return re.sub("https?://.*reddit\.com", "https://np.reddit.com", url)
 
 def get_new_submissions(reddit, db):
-    """Get newest submissions, add them as pending in database and return Submission objects."""
-    new_submissions = reddit.get_subreddit(CONFIG["subreddit"]).get_new(limit=CONFIG["result_limit"])
+    """Get newest submissions, add them as pending in database and return
+    Submission objects."""
+    limit = C["result_limit"]
+    subreddit = C["subreddit"]
+    new_submissions = reddit.get_subreddit(subreddit).get_new(limit=limit)
     submissions = [x for x in new_submissions]
     [db.add_submission(x) for x in submissions]
     return submissions
 
 def get_new_comments(reddit):
     """Get newest comments in subreddit."""
-    subreddit = reddit.get_subreddit(CONFIG["subreddit"])
-    return subreddit.get_comments(limit=CONFIG["result_limit"])
+    subreddit = reddit.get_subreddit(C["subreddit"])
+    return subreddit.get_comments(limit=C["result_limit"])
 
 def take_screenshot(url, name):
     """Take screenshot of url and save to file, return path."""
-    #expand_comments = 'var el=$(".bylink");var ei=0;(function fe(){while(ei<el.length){var s=el[ei].innerHTML;var parent=s.indexOf("parent");if(parent==0){showcomment(el[ei]);}ei++;}})()'
-    driver = webdriver.PhantomJS(executable_path=CONFIG["phantomjs_exe"])
-    filename = os.path.join(CONFIG["image_dir"], "%s.png" % (name))
-    driver.set_window_size(*CONFIG["viewport"])
+    driver = webdriver.PhantomJS(executable_path=C["phantomjs_exe"])
+    filename = os.path.join(C["image_dir"], "%s.png" % (name))
+    driver.set_window_size(*C["viewport"])
     logging.info("Taking screenshot of %s (%s)", name, url)
     driver.get(url)
     #driver.execute_script(expand_comments)
@@ -258,50 +241,57 @@ def take_screenshot(url, name):
 
 def upload_screenshot(filename):
     """Upload image file to imgur, return url."""
-    imgur = pyimgur.Imgur(client_id=CONFIG["imgur_api_key"])
+    imgur = pyimgur.Imgur(client_id=C["imgur_api_key"])
     uploaded = imgur.upload_image(filename)
     return uploaded.link
 
 def crop_screenshot(filename):
     """Crop a screenshot to configured height, save to file and return path."""
     screenshot = Image.open(filename)
-    cropped = screenshot.crop((0, 0, CONFIG["viewport"][0], CONFIG["cropped_height"]))
+    to_height = C["cropped_height"]
+    cropped = screenshot.crop((0, 0, C["viewport"][0], to_height))
     cropped_filename = re.sub("\.png$", "-cropped.png", filename)
     cropped.save(cropped_filename)
+    screenshot.close()
+    cropped.close()
     return cropped_filename
 
 def check_messages(reddit, db):
     """Get unread bot messages/commands and process."""
     # get unread messages and mark read/clear notice
     messages = reddit.get_unread(True, True)
-    subreddit = reddit.get_subreddit(CONFIG["subreddit"])
+    subreddit = reddit.get_subreddit(C["subreddit"])
     mod_list = [x.name for x in subreddit.get_moderators()]
 
     for message in messages:
         # don't process non-direct pm or already processed ones
-        if not db.comment_already_done("pm_" + message.name) and message.context == "":
+        if (not db.comment_already_done("pm_" + message.name)
+            and message.context == ""):
             # check user is privileged, ignore if not
             if not message.author.name in mod_list:
-                logging.warning("Unauthorised user %s tried to run command", message.author.name)
+                logging.warning("Unauthorised user %s tried to run command",
+                                message.author.name)
                 db.add_comment("pm_" + message.name)
                 continue
 
             # process command
-            # NOTE: if this needs to expand it must be split up
+            subject = T["ban_subject"]
             try:
                 msg = message.body.split()
                 if msg[0] == "ban":
                     # ban specified user
                     db.set_user_status(msg[1], "banned")
                     logging.info("%s has been shadowbanned", msg[1])
-                    subreddit.send_message(TEMPLATES["ban_subject"],
-                                           TEMPLATES["ban_body"] % (msg[1], message.author.name))
+                    body = T["ban_body"] % (msg[1],
+                                            message.author.name)
+                    subreddit.send_message(subject, body)
                 elif msg[0] == "unban":
                     # unban specified user
                     db.set_user_status(msg[1], "pass")
                     logging.info("%s has been unshadowbanned", msg[1])
-                    subreddit.send_message(TEMPLATES["ban_subject"],
-                                           TEMPLATES["unban_body"] % (msg[1], message.author.name))
+                    body = T["unban_body"] % (msg[1],
+                                              message.author.name)
+                    subreddit.send_message(subject, body)
                 elif msg[0] == "showbans":
                     # send list of all bans to modmail
                     user_list = ""
@@ -311,10 +301,12 @@ def check_messages(reddit, db):
                         user_list = "no bans"
 
                     logging.info("Posting ban list")
-                    subreddit.send_message(TEMPLATES["ban_subject"],
-                                           TEMPLATES["showbans_body"] % (message.author.name, user_list))
+                    body = T["showbans_body"] % (message.author.name,
+                                                 user_list)
+                    subreddit.send_message(subject, body)
             except:
-                # not really fatal if this happens, can simply be logged and skipped
+                # not really fatal if this happens,
+                # can simply be logged and skipped
                 logging.exception("Error occurred when process command list")
             finally:
                 db.add_comment("pm_" + message.name)
@@ -325,20 +317,22 @@ def check_user(db, session, name, subreddit, url=""):
     # only warns once
     if not db.user_already_checked(name):
         redditor = session.get_redditor(name)
-        limit_seconds = CONFIG["sketchy_days"] * 24 * 60 * 60
+        limit_seconds = C["sketchy_days"] * 24 * 60 * 60
 
         # check if sketchy
         if (time.time() - redditor.created) < limit_seconds:
-            karma = CONFIG["sketchy_karma"]
+            karma = C["sketchy_karma"]
             # check for low karma
-            if redditor.link_karma < karma and redditor.comment_karma < karma:
+            if (redditor.link_karma < karma
+                and redditor.comment_karma < karma):
                 logging.info("%s is sketchy", redditor.name)
                 # add to database sketchy
                 db.add_user(redditor.name, "sketchy")
                 # send mod mail
-                subreddit.send_message(TEMPLATES["sketchy_subject"],
-                                       TEMPLATES["sketchy_body"] % (redditor.name, redditor._url,
-                                                                    url, redditor.name))
+                body = T["sketchy_body"] % (redditor.name,
+                                            redditor._url,
+                                            url, redditor.name)
+                subreddit.send_message(T["sketchy_subject"], body)
                 logging.info("Notified mods")
         else:
             logging.info("%s is cool", redditor.name)
@@ -362,13 +356,15 @@ def mod_submission(db, new):
         return
 
     # lookup and index account, warn mods if account is suspicious
-    check_user(db, new.reddit_session, new.author.name, new.subreddit, new.permalink)
+    check_user(db, new.reddit_session, new.author.name,
+               new.subreddit, new.permalink)
 
     # remove submission silently if user is shadowbanned
     if db.user_is_banned(new.author.name):
-        if not CONFIG["testing"]:
+        if not C["testing"]:
             new.remove()
-        logging.info("Removed submission, %s is shadowbanned", new.author.name)
+        logging.info("Removed submission, %s is shadowbanned",
+                     new.author.name)
         db.set_submission_status(new.name, "complete")
         return
 
@@ -379,18 +375,12 @@ def mod_submission(db, new):
 
         try:
             # notify user
-            if not CONFIG["testing"]:
-                new.add_comment(TEMPLATES["non-np_comment"] % to_np(new.url))
+            if not C["testing"]:
+                new.add_comment(T["non-np_comment"] % to_np(new.url))
             logging.info("Notified user of non-np link")
 
-            # send mod mail
-            # NOTE: disabled for now, don't really care
-            #new.subreddit.send_message(TEMPLATES["non-np_subject"],
-            #                           TEMPLATES["non-np_body"] % (new.permalink))
-            #logging.info("Notified mods")
-
             # remove the submission
-            if not CONFIG["testing"]:
+            if not C["testing"]:
                 new.remove()
             logging.info("Removed submission")
 
@@ -418,14 +408,28 @@ def mod_comment(db, comment):
 
     # shadowban
     if db.user_is_banned(comment.author.name):
-        if not CONFIG["testing"]:
+        if not C["testing"]:
             comment.remove()
         logging.info("Removed comment, %s is shadowbanned", comment.author.name)
 
     db.add_comment(comment.name)
 
+def do_html_dump(url, name):
+    """Download a copy of page and write to unique file."""
+    headers = {"User-Agent": C["user_agent"]}
+    req = urllib.request.Request(url, headers=headers)
+    html = urllib.request.urlopen(req).read()
+
+    filename = name + "-" + str(int(time.time())) + ".html"
+    dump = open(os.path.join(C["html_dump_dir"], filename), "w")
+    dump.write(str(html))
+    dump.close()
+
+    return filename
+
 def try_screenshot(db, new):
-    """If required, take a screenshot of submission link and post comment to uploaded image."""
+    """If required, take a screenshot of submission link and post comment
+    to uploaded image."""
     # bail out if we already did this one
     if db.already_done(new.name):
         logging.debug("Skip screenshot %s", new.name)
@@ -440,12 +444,13 @@ def try_screenshot(db, new):
     # take and save the screenshot
     try:
         screenie = take_screenshot(new.url, new.name)
+        original = screenie
     except:
         logging.exception("Error while taking screenshot of %s", new.url)
         return
 
     # crop it, if required
-    if os.path.getsize(screenie) > CONFIG["imgur_max_size"]:
+    if os.path.getsize(screenie) > C["imgur_max_size"]:
         # TODO: this method of cropping is not totally reliable and can result
         # in a screenshot being cropped larger than it was, filled with white
         # space. this happens on subs that have complex themes
@@ -460,16 +465,39 @@ def try_screenshot(db, new):
     # upload to imgur
     try:
         imgur = upload_screenshot(screenie)
+        db.set_submission_status(new.name, "uploaded")
     except:
         logging.exception("Could not upload %s", screenie)
+        db.set_submission_status(new.name, "fail-upload")
+        imgur = False
+
+    if C["testing"]:
+        db.set_submission_status(new.name, "complete-test")
         return
-    db.set_submission_status(new.name, "uploaded")
 
     # post the link in the submission thread
     try:
-        if not CONFIG["testing"]:
-            new.add_comment(TEMPLATES["screenshot_comment"] % imgur)
-        logging.info("Linked screenshot in submission")
+        if C["use_random_quote"]:
+            quote = grab_quote()
+        else:
+            quote = "post was archived!"
+
+        local_addr = "%s/%s" % (C["image_dir_url"],
+                                os.path.basename(original))
+
+        # workaround for if imgur upload fails
+        if not imgur:
+            imgur = local_addr
+
+        html_dump = do_html_dump(new.url, new.name)
+        html_dump_addr = "%s/%s" % (C["dump_dir_url"],
+                                    html_dump)
+
+        comment = T["screenshot_comment"] % (quote, imgur,
+                                             local_addr,
+                                             html_dump_addr)
+        new.add_comment(comment)
+        logging.info("Added comment")
     except:
         logging.exception("Error adding comment to %s", new.permalink)
         db.set_submission_status(new.name, "failed")
@@ -484,14 +512,14 @@ def jerk_run():
 
     logging.info("=== jerkbot init ===")
 
-    if CONFIG["testing"]:
-        logging.warning("!!! Testing mode is enabled, nothing much will happen !!!")
+    if C["testing"]:
+        logging.warning("!!! Testing mode is enabled, nothing much will happen")
 
     # start new reddit session
     logging.info("*** Init reddit session")
     try:
-        reddit = praw.Reddit(user_agent=CONFIG["user_agent"])
-        reddit.login(CONFIG["reddit_username"], CONFIG["reddit_password"])
+        reddit = praw.Reddit(user_agent=C["user_agent"])
+        reddit.login(C["reddit_username"], C["reddit_password"])
     except:
         logging.exception("Unable to connect to reddit")
         return
@@ -524,6 +552,7 @@ def jerk_run():
             mod_comment(db, comment)
 
     logging.info("=== jerkbot run complete ===")
+
 
 if __name__ == "__main__":
     jerk_run()
